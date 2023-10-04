2780734604af073f56bd2525a7cd20a11e113155
Carlos Kelley <carloskelley@protonmail.com>
2023-10-03 23:13:16 -0500

    cards are draggable. working on processing collision.

--- card.gd
+++ card.gd
@@ -5,16 +5,29 @@ var radius: int = 30  # Radius of the circle
 var in_hand: bool = true  # To determine if the card is still in a player's hand
 var font = ThemeDB.fallback_font
 
+var is_dragging = false
+var drag_offset = Vector2()
 
 
 signal card_clicked(card_node)
+signal card_dropped(card_node)
+
 
 func _input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		var mouse_pos = get_global_mouse_position()
-		var distance_to_center = mouse_pos.distance_to(global_position)
-		if distance_to_center <= radius:
-			emit_signal("card_clicked", self)
+	if in_hand:
+		if event is InputEventMouseButton:
+			var mouse_pos = get_global_mouse_position()
+			var distance_to_center = mouse_pos.distance_to(global_position)
+			if distance_to_center <= radius:
+				if event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+					is_dragging = true
+					drag_offset = mouse_pos - global_position
+				elif not event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+					is_dragging = false
+					emit_signal("card_dropped", self)
+		elif event is InputEventMouseMotion and is_dragging:
+			global_position = event.global_position - drag_offset
+
 
 
 
--- game.gd
+++ game.gd
@@ -22,6 +22,43 @@ func _ready():
 	print("P1 Adj", player1_adjectives)
 	print("P2 Adj", player2_adjectives)
 
+	# Connect the card_dropped signal for each card
+	for card in get_tree().get_nodes_in_group("cards"): # Make sure your cards are in a group named "cards"
+		card.connect("card_dropped", self._on_card_dropped)
+
+func _on_card_dropped(card_node):
+	# Check collision with lane 1
+	if collide_card_with_lane(card_node, $Lane1Collision):
+		print("Card dropped in Lane 1")
+		move_card_to_lane(card_node, lane1_position)
+
+	# Check collision with lane 2
+	if collide_card_with_lane(card_node, $Lane2Collision):
+		print("Card dropped in Lane 2")
+		move_card_to_lane(card_node, lane2_position)
+
+func collide_card_with_lane(card, lane_collision):
+	var card_radius = card.radius  # Accessing the radius of the card
+
+	var card_shape = card.get_world_2d().direct_space_state.create_convex_polygon([
+		card.global_position + Vector2(-card_radius, -card_radius),
+		card.global_position + Vector2(card_radius, -card_radius),
+		card.global_position + Vector2(card_radius, card_radius),
+		card.global_position + Vector2(-card_radius, card_radius)])
+
+	var shape_query = PhysicsShapeQueryParameters2D.new()
+	shape_query.shape = card_shape
+	shape_query.transform = Transform2D(0, card.global_position)
+
+	var results = get_world_2d().direct_space_state.intersect_shape(shape_query)
+	for result in results:
+		if result.collider == lane_collision:
+			return true
+
+	return false
+
+
+
 func generate_adjectives(player):
 	var player_adjectives = []
 
