3956fd3335586a27980c280ec7c8be10a9e01ec2
Carlos Kelley <carloskelley@protonmail.com>
2024-04-12 08:03:15 -0500

    feat: adj on screen

--- adjectives/adjective.gd
+++ adjectives/adjective.gd
@@ -1,13 +1,13 @@
 class_name Adjective
 extends Node2D
 
-
 var description: String = ""
 var effect: Effect = NoneEffect.new()
 var scope: Scope = NoneScope.new()
 
 
 func _ready() -> void:
+	print("Adjective ready", name)
 	var label: Label = Label.new()
 	label.text = name
 	add_child(label)
--- managers/adjective_manager.gd
+++ managers/adjective_manager.gd
@@ -7,14 +7,16 @@ var adjectives: Array = [Jealous]
 #print tree
 
 
-
 func generate_adjectives() -> Array:
 	print("Generating adjectives with game", game)
 	# Actually create instances of the adjectives
 	var random_adjectives: Array = []
 	for i: int in range(adjectives.size()):
 		var adjective_instance: Adjective = adjectives[i].new()
-		# game.add_child(adjective_instance)
+		# var adjective_scene = preload("res://Adjective.tscn")
+		game.add_child(adjective_instance)
+		
+		# adjective_instance.add_child(adjective_scene)
 		random_adjectives.append(adjective_instance)
 	print("Generated adjs: ", random_adjectives)
 	return random_adjectives
